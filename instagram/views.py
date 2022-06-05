1aabad9a02c84f5b55355e4448ff41aaacb5384a
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-05 20:22:29 +0300

    add forms.py and forms in register.html

--- instagram/views.py
+++ instagram/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+
+from .forms import CreateUserForm
+from .models import *
 
 # Create your views here.
 def login(request):
     return render(request,'login.html')
 
 def register(request):
-    form = UserCreationForm()
-    
+    form = CreateUserForm()
+
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
 
