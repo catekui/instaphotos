2ea7437a345dae654958aad9f95c8cb3a5c209ea
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-05 20:38:53 +0300

    styled register.html

--- instagram/forms.py
+++ instagram/forms.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 
-
-class CreateUserForm(UserCreationForm()):
+class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
