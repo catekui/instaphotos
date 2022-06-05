5b03d0825d685c2c2124d0c3685ce6146e9797d3
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-05 21:52:23 +0300

    redirected register.html

--- instagram/views.py
+++ instagram/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib import messages
+
 from .forms import CreateUserForm
 from .models import *
 
@@ -16,6 +18,10 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user)
+
+            return redirect('login')
 
     context = {'form': form}
     return render(request,'register.html', context)
