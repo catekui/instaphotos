28108d53858d7fecec7982826d132525b64c0a1f
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-06 17:40:09 +0300

    added image model

--- instagram/forms.py
+++ instagram/forms.py
@@ -10,3 +10,9 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
+class PictureUploadForm(ModelForm):
+    class Meta:
+        model = Image
+        fields =['image_name','image_caption','image']
+
--- instagram/models.py
+++ instagram/models.py
@@ -3,7 +3,18 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 
+class Image(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images',null=True,blank="True")
+    image_name = models.CharField(max_length=100)
+    image_caption = models.TextField(max_length=1000)
+    image = CloudinaryField('image')
+    profile = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank="True")
+    likes_number = models.IntegerField(default=0)
+    comments_number = models.IntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.image_name
 
 
 class Profile(models.Model):
@@ -12,7 +23,16 @@ class Profile(models.Model):
     profile_photo = CloudinaryField('image')
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.user
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=250)
+    created_on = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return self.image_name
+        return self.comment
