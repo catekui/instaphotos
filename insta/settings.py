95df02c2a7ee30d18040cec90213962a465876ec
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-08 01:01:00 +0300

    updated the settings

--- insta/settings.py
+++ insta/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = Path(_file_).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
